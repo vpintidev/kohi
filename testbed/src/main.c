498ea1eaf96563ebe95b11386f2c54ab6e28f277
vpinti <vittorio.pinti@gmail.com>
2026-08-06 21:06:21 +0200

    Logging and assertion

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,8 +1,16 @@
-#include <test.h>
+#include "core/assert.h"
+#include <core/logger.h>
 
 int main(void)
 {
-  print_int(42);
+  KFATAL("A test message: %f", 3.14f);
+  KERROR("A test message: %f", 3.14f);
+  KWARN("A test message: %f", 3.14f);
+  KINFO("A test message: %f", 3.14f);
+  KDEBUG("A test message: %f", 3.14f);
+  KTRACE("A test message: %f", 3.14f);
+
+  KASSERT(1 == 0);
 
   return 0;
 }
