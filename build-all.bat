84e54551b67f77ea3ac930f67c96d8280e8944c0
vpinti <vittorio.pinti@gmail.com>
2026-08-10 19:00:49 +0200

    Windows platform

--- build-all.bat
+++ build-all.bat
@@ -3,16 +3,18 @@ REM Build Everything
 
 ECHO "Building everything..."
 
+REM Create bin directory if it doesn't exist
+IF NOT EXIST bin MKDIR bin
 
 PUSHD engine
 CALL build.bat
 POPD
-IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit /b %ERRORLEVEL%)
 
 PUSHD testbed
 CALL build.bat
 POPD
-IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit /b %ERRORLEVEL%)
 
 ECHO "All assemblies built successfully."
 
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include "core/assert.h"
 #include <core/logger.h>
 
+// TODO: Test
+#include <platform/platform.h>
+
 int main(void)
 {
   KFATAL("A test message: %f", 3.14f);
@@ -10,7 +13,13 @@ int main(void)
   KDEBUG("A test message: %f", 3.14f);
   KTRACE("A test message: %f", 3.14f);
 
-  KASSERT(1 == 0);
+  platform_state state;
+  if(platform_startup(&state, "Kohi Engine Testbed", 100, 100, 1280, 720)) {
+    while(TRUE) {
+      platform_pump_messages(&state);
+    }
+  }
 
+  platform_shutdown(&state);
   return 0;
 }
