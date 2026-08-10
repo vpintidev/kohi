2340781ef6ca37e8a4e47f3311fb79f6b8d682d0
vpinti <vittorio.pinti@gmail.com>
2026-08-10 19:06:00 +0200

    fix return type platform_free function

--- engine/src/platform/platform.h
+++ engine/src/platform/platform.h
@@ -19,7 +19,7 @@ KAPI void platform_shutdown(platform_state* plat_state);
 KAPI b8 platform_pump_messages(platform_state* plat_state);
 
 void* platform_allocate(u64 size, b8 aligned);
-void* platform_free(void* block, b8 aligned);
+void platform_free(void* block, b8 aligned);
 void* platform_zero_memory(void* block, u64 size);
 void* platform_copy_memory(void* dest, const void* source, u64 size);
 void* platform_set_memory(void* dest, i32 value, u64 size);
